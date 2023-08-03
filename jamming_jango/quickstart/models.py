0dd03784d634b766259525638e18b53b918ebf75
Parker S <111610335+Psenter@users.noreply.github.com>
2023-08-03 14:34:46 +0000

    Finished 1st itteration of models.

--- jamming_jango/quickstart/models.py
+++ jamming_jango/quickstart/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Song(models.Model):
+    title = models.CharField(max_length=200)
+    album_id = models.ForeignKey()
+    artist_id = models.ForeignKey()
+
+    def __str__(self):
+        return self.title
+    
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+    
+class Genre(models.Model):
+    type = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.type
+
+class Album(models.Model):
+    name = models.CharField(max_length=200)
+    release_date = models.CharField(max_length=200)
+    rating = models.CharField(max_length=200)
+    artist_id = models.ForeignKey()
+
+    def __str__(self):
+        return f"{self.name} - {self.release_date} - {self.rating}"
