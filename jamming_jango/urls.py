788b6139a09bbe99e9be5ed94001b86f07aa47c4
Parker S <111610335+Psenter@users.noreply.github.com>
2023-08-03 21:34:16 +0000

    Started Urls and got some routes done.

--- jamming_jango/urls.py
+++ jamming_jango/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from quickstart import views
+
+router = routers.DefaultRouter()
+router.register(r'songs', views.SongViewSet)
+router.register(r'artists', views.ArtistViewSet)
+router.register(r'genre', views.GenreViewSet)
+router.register(r'album', views.AlbumViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
