7a17532b36bdb105ca7bd7fa7b5c6b20a32ab9fb
Parker S <111610335+Psenter@users.noreply.github.com>
2023-08-04 13:05:11 +0000

    Cleaned up file.

--- quickstart/views.py
+++ quickstart/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from rest_framework import permissions
 from .serializers import SongSerializer, ArtistSerializer, GenreSerializer, AlbumSerializer
 from .models import Song, Artist, Genre, Album
 
